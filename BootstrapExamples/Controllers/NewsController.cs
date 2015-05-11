8a56df733c988d4c0afa56e04318ba47db14e87f
mkm_feel <mkmfeel@gmail.com>
2015-05-11 13:05:12 +0300

    Новость (начало)

---
+++ BootstrapExamples/Controllers/NewsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace BootstrapExamples.Controllers
+{
+    public class NewsController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Event(int id)
+        {
+            return View();
+        }
+    }
+}
